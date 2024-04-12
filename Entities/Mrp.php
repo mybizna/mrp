c88de3aaf625141738932447f1d0f69063426a04
Dedan <dedanirungu@gmail.com>
2024-04-12 10:01:21 +0300

    Add rights() method to Mrp.php

--- Entities/Mrp.php
+++ Entities/Mrp.php
@@ -48,4 +48,15 @@ class Mrp extends BaseModel
         $this->fields->increments('id')->html('hidden');
         $this->fields->string('name')->html('text');
     }
+
+
+    /**
+     * Define rights for this model.
+     *
+     * @return array
+     */
+    public function rights(): array
+    {
+
+    }
 }
