c968f918b5e795e9a697ec10a7d0489c5441c6ff
Dedan <dedanirungu@gmail.com>
2024-04-12 11:14:43 +0300

    Add rights() method to Mrp.php

--- Entities/Mrp.php
+++ Entities/Mrp.php
@@ -57,6 +57,12 @@ class Mrp extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 }
