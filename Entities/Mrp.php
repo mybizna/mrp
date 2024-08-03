3d77f7203b42c3f79269eec21844cef1572d64f3
Dedan <dedanirungu@gmail.com>
2024-08-03 11:56:01 +0300

    Changes to files: Entities/Mrp.php

--- Entities/Mrp.php
+++ Entities/Mrp.php
@@ -15,20 +15,6 @@ class Mrp extends BaseModel
     protected $fillable = ['name'];
 
     /**
-     * The fields that are to be render when performing relationship queries.
-     *
-     * @var array<string>
-     */
-    public $rec_names = ['name'];
-
-    /**
-     * List of tables names that are need in this model during migration.
-     *
-     * @var array<string>
-     */
-    public array $migrationDependancy = [];
-
-    /**
      * The table associated with the model.
      *
      * @var string
@@ -50,19 +36,5 @@ class Mrp extends BaseModel
     }
 
 
-    /**
-     * Define rights for this model.
-     *
-     * @return array
-     */
-    public function rights(): array
-    {
-        $rights = parent::rights();
 
-        $rights['staff'] = ['view' => true];
-        $rights['registered'] = ['view' => true];
-        $rights['guest'] = [];
-
-        return $rights;
-    }
 }
